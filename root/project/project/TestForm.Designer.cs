21b3c902c42bcc442f4196e2569cff8b3fcddaae
nakedsnake.boschuk <nakedsnake.boschuk@59bf14ac-0cb7-11de-a021-8b77bb4f2096>
2009-03-12 21:53:42 +0000

    Проблемы с отрисовкой движения , в процессе разработки

--- root/project/project/TestForm.Designer.cs
+++ root/project/project/TestForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.panelDrawingArea = new System.Windows.Forms.Panel();
+            this.buttonTry = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panelDrawingArea
@@ -39,11 +40,22 @@
             this.panelDrawingArea.Size = new System.Drawing.Size(656, 296);
             this.panelDrawingArea.TabIndex = 0;
             // 
+            // buttonTry
+            // 
+            this.buttonTry.Location = new System.Drawing.Point(13, 332);
+            this.buttonTry.Name = "buttonTry";
+            this.buttonTry.Size = new System.Drawing.Size(75, 23);
+            this.buttonTry.TabIndex = 1;
+            this.buttonTry.Text = "Try To Move";
+            this.buttonTry.UseVisualStyleBackColor = true;
+            this.buttonTry.Click += new System.EventHandler(this.buttonTry_Click);
+            // 
             // TestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(694, 503);
+            this.Controls.Add(this.buttonTry);
             this.Controls.Add(this.panelDrawingArea);
             this.Name = "TestForm";
             this.Text = "TestForm";
@@ -54,5 +66,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panelDrawingArea;
+        private System.Windows.Forms.Button buttonTry;
     }
 }
--- root/project/project/TestForm.cs
+++ root/project/project/TestForm.cs
@@ -14,32 +14,42 @@ namespace project
         Bitmap lBitmapdrawingArea;
         Graphics DrawingArea;
 
-        
+
         System.Collections.Generic.List<DrawingRectangle> Groop = new List<DrawingRectangle>(10);
 
         DrawingRectangle exTank;
-       
+
 
         public TestForm()
         {
             InitializeComponent();
-            
 
             DrawingArea = panelDrawingArea.CreateGraphics();
 
-            Point startPoint = new Point(50,panelDrawingArea.Height- 50);
-            exTank = new DrawingRectangle(DrawingArea, startPoint, 250, 150);
+            Point startPoint = new Point(50,  panelDrawingArea.Height - 50);
+            exTank = new DrawingRectangle(DrawingArea, startPoint, 25, 15);
             Groop.Add(exTank);
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            
+
             for (int i = 0; i < Groop.Count; i++)
             {
                 Groop[i].Draw();
             }
+        }
+
+        private void buttonTry_Click(object sender, EventArgs e)
+        {
+            DrawingRectangle D =  Groop[0];
+            D.startMoving(D.BodyPoint.X,D.BodyPoint.Y,75,75);
+
+            PaintEventArgs ee = new PaintEventArgs(DrawingArea,new Rectangle(0,0,panelDrawingArea.Width,panelDrawingArea.Height));
+
+            OnPaint(ee);
+            MessageBox.Show("Complite");
         }
     }
 
@@ -49,7 +59,15 @@ namespace project
     public class DrawingRectangle
     {
         private string name; // имя обьекта
-        private Point startPoint;//точка местоположения - левая нижняя
+        private Point bodyPoint;//точка местоположения - левая нижняя
+
+        public Point BodyPoint
+        {
+            get
+            {
+                return bodyPoint;
+            }
+        }
 
         private Graphics drawPlace;  //место где рисуется
         public string Name
@@ -68,15 +86,15 @@ namespace project
 
         private Pen pen; // контур
         private Brush brush = Brushes.Blue; // для заливки
-      
-        public DrawingRectangle(Graphics place, Point pointWhere ,int xSize, int ySize)
+
+        public DrawingRectangle(Graphics place, Point pointWhere, int xSize, int ySize)
         {
-            pen = new Pen(Brushes.Blue) ;
+            pen = new Pen(Brushes.Blue);
             drawPlace = place;
 
-            startPoint.X = pointWhere.X;
-            startPoint.Y = pointWhere.Y - ySize;
-            
+            bodyPoint.X = pointWhere.X;
+            bodyPoint.Y = (pointWhere.Y - ySize);
+
             //поскольку для инициализации берется левая верхняя , немного пошаманим
             body = new Rectangle(pointWhere.X, pointWhere.Y - ySize, xSize, ySize);
 
@@ -97,73 +115,143 @@ namespace project
         /// <param name="yEnd">конечная координата y</param>
         public void startMoving(int xStart, int yStart, int xEnd, int yEnd)
         {
-            
-        }
-
-    }
-    /// <summary>
-    /// Инкапсулирует набор рисуемых прямоугольников
-    /// </summary>
-    public class DrawingFigure
-    {
-        /// <summary>
-        /// Начальная точка - левая нижняя
-        /// </summary>
-        private Point Position;  //определить за начальную точку брать 
-        private Graphics DrawPlace;     
-
-        const int Lsize = 40;
-
-        private Pen pen = new Pen(Brushes.Blue); // контур
-
-        private Brush brush = Brushes.Blue; // для заливки
-        private Brush textcolor = Brushes.Red;
-        private Rectangle body = new Rectangle(0, 0, 2 * Lsize, Lsize);
-        
-
-        public DrawingFigure(int xPos, int yPos, Graphics WhereDraw)
-        {
-            Position.X = xPos;
-            Position.Y = yPos;
-           
-            DrawPlace = WhereDraw;
-        }
-
-        public void Draw(int x, int y)
-        {
-            body = new Rectangle(x, y, 2 * Lsize, Lsize);
-            DrawPlace.DrawRectangle(pen, body);
-            DrawPlace.FillRectangle(brush, body);
-
-        }
-        public void Draw()
-        {
-            body = new Rectangle(Position.X, Position.Y, 2 * Lsize + 10, Lsize);
-
-            DrawPlace.DrawRectangle(pen, body);
-            DrawPlace.FillRectangle(brush, body);
-
-        }
-        public void Draw(bool IsMoving)
-        {
-            body = new Rectangle(Position.X, Position.Y, 2 * Lsize + 10, Lsize);
-            if (IsMoving == false)
+#warning добавить смену экранов?
+            //int xStart, yStart, xEnd, yEnd;
             {
-                DrawPlace.DrawRectangle(pen, body);
-                DrawPlace.FillRectangle(textcolor, body);
+                int dx, dy, s;
+
+                /* Упорядочивание координат и вычисление приращений */
+                if (xStart > xEnd)
+                {
+                    s = xStart; xStart = xEnd; xEnd = s;
+                    s = yStart; yStart = yEnd; yEnd = s;
+                }
+                dx = xEnd - xStart; dy = yEnd - yStart;
+
+                /* Занесение начальной точки вектора */
+                //   PutPixLn (xn, yn, Pix_C);
+                bodyPoint.X = xStart;
+                bodyPoint.Y = yStart;
+
+                Draw();
+
+                if (dx == 0 && dy == 0) return;
+                /* Вычисление количества позиций по X и Y */
+                dx = dx + 1; dy = dy + 1;
+
+                /* Собственно генерация вектора */
+                if (dy == dx)
+                {                 /* Наклон == 45 градусов */
+                    while (xStart < xEnd)
+                    {
+                        xStart = xStart + 1;
+                        //PutPixLn (xn, xn, Pix_C);
+                        bodyPoint.X = xStart;
+                        bodyPoint.Y = yStart;
+                        Draw();
+                    }
+                }
+                else if (dx > dy)
+                {           /* Наклон <  45 градусов */
+                    s = 0;
+                    while (xStart < xEnd)
+                    {
+                        xStart = xStart + 1;
+                        s = s + dy;
+                        if (s >= dx)
+                        {
+                            s = s - dx; yStart = yStart + 1;
+                        }
+                        //PutPixLn (xn, yn, Pix_C);
+                        bodyPoint.X = xStart;
+                        bodyPoint.Y = yStart;
+                        Draw();
+                    }
+                }
+                else
+                {                        /* Наклон >  45 градусов */
+                    s = 0;
+                    while (yStart < yEnd)
+                    {
+                        yStart = yStart + 1;
+                        s = s + dx;
+                        if (s >= dy)
+                        {
+                            s = s - dy; xStart = xStart + 1;
+                        }
+                        //PutPixLn (xn, yn, Pix_C);
+                        bodyPoint.X = xStart;
+                        bodyPoint.Y = yStart;
+                        Draw();
+                    }
+                }
             }
-            else
+
+        }
+        /// <summary>
+        /// Инкапсулирует набор рисуемых прямоугольников
+        /// </summary>
+        public class DrawingFigure
+        {
+            /// <summary>
+            /// Начальная точка - левая нижняя
+            /// </summary>
+            private Point Position;  //определить за начальную точку брать 
+            private Graphics DrawPlace;
+
+            const int Lsize = 40;
+
+            private Pen pen = new Pen(Brushes.Blue); // контур
+
+            private Brush brush = Brushes.Blue; // для заливки
+            private Brush textcolor = Brushes.Red;
+            private Rectangle body = new Rectangle(0, 0, 2 * Lsize, Lsize);
+
+
+            public DrawingFigure(int xPos, int yPos, Graphics WhereDraw)
             {
+                Position.X = xPos;
+                Position.Y = yPos;
+
+                DrawPlace = WhereDraw;
+            }
+
+            public void Draw(int x, int y)
+            {
+                body = new Rectangle(x, y, 2 * Lsize, Lsize);
                 DrawPlace.DrawRectangle(pen, body);
                 DrawPlace.FillRectangle(brush, body);
+
             }
+            public void Draw()
+            {
+                body = new Rectangle(Position.X, Position.Y, 2 * Lsize + 10, Lsize);
 
+                DrawPlace.DrawRectangle(pen, body);
+                DrawPlace.FillRectangle(brush, body);
+
+            }
+            public void Draw(bool IsMoving)
+            {
+                body = new Rectangle(Position.X, Position.Y, 2 * Lsize + 10, Lsize);
+                if (IsMoving == false)
+                {
+                    DrawPlace.DrawRectangle(pen, body);
+                    DrawPlace.FillRectangle(textcolor, body);
+                }
+                else
+                {
+                    DrawPlace.DrawRectangle(pen, body);
+                    DrawPlace.FillRectangle(brush, body);
+                }
+
+            }
         }
-    }
 
-   /* public class Apc : DrawingFigure
-    {
+        /* public class Apc : DrawingFigure
+         {
         
+         }
+         * */
     }
-    * */
 }
